0dca183d2e6ef15594e2892a287c337f9c841279
Vinicius Goulart Alves <vini.goulartalves@gmail.com>
2024-02-02 17:50:08 -0300

    Revisão 2

--- src/ListaCursosProfessores.java
+++ src/ListaCursosProfessores.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class ListaCursoProfessores {
+public class ListaCursosProfessores {
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
